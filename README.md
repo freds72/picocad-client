0fd67db83570768953967ef8196dd0a72205f9b3
freds <frederic.souchu@laposte.net>
2021-02-27 21:09:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# PicoCAD Client
+
+Starter kit 3d engine to display PicoCAD models:
+
+- basic FPS controller
+- perspective camera
+- optimized transform & project pipeline
+- global sorting
+
+![Tank Prototype](chunky_tanks.gif)
+
+>Note: sample code requires [Pico8](https://www.lexaloffle.com/pico-8.php)
+
+Many points are left to the reader:
+- efficient culling of non visible objects
+- near plane clipping
+- collision & anything related to a particular game
+
+## How To Use
+- Edit model using [PicoCAD](https://johanpeitz.itch.io/picocad)
+- Export model to p8l file
+- Edit p8l file to remove comments
+- Copy texture image in picocad_player.p8 cart
+- Edit Include p8l file in picocad_player.p8. Ex:
+```
+#include chunky_tank.p8l
+```
+- Create 3d entity in _init with:
+```
+  _entities={
+    -- initial (x,y,z) position + reference to 3d model  
+    -- note: y is up  
+    make_3dobject({5,0,5},chunky_tank)
+  }
+```
+- Change game logic/structure to fit your game!
+- Good luck
+
+## Licence
+
+Sample code is licenced under: [Creative Commons Attribution 4.0 International Public License](https://creativecommons.org/licenses/by/4.0/legalcode)
+
---
+++ carts/poly.lua
@@ -0,0 +1,84 @@
+function polyfill(v,c)	
+	if(#v<3) return
+	color(c)
+	local p0,spans=v[#v],{}
+	local x0,y0=p0.x,p0.y
+	-- ipairs is slower for small arrays
+	for i=1,#v do
+		local p1=v[i]
+		local x1,y1=p1.x,p1.y
+		local _x1,_y1,_v1=x1,y1
+		if(y0>y1) x0,y0,x1,y1=x1,y1,x0,y0
+		local dx=(x1-x0)/(y1-y0)
+		local cy0=y0\1+1
+		if(y0<0) x0-=y0*dx y0=0 cy0=0
+		-- sub-pix shift
+		x0+=(cy0-y0)*dx
+		if(y1>127) y1=127
+		for y=cy0,y1 do
+			local span=spans[y]
+			if span then
+				rectfill(x0,y,span,y)
+			else
+				spans[y]=x0
+			end
+			x0+=dx
+		end
+		x0,y0=_x1,_y1
+	end
+end
+
+function polyline(v,c)
+	color(c)
+	local nv=#v
+	for i,p1 in pairs(v) do
+		local p0=v[i%nv+1]
+		line(p0.x,p0.y,p1.x,p1.y)
+	end
+end
+
+function tpoly(v,uv)
+	local p0,spans=v[#v],{}
+	local x0,y0=p0.x,p0.y
+	local u0,v0=uv[#uv-1],uv[#uv]
+	-- ipairs is slower for small arrays
+	for i=1,#v do
+		local p1=v[i]
+		local x1,y1=p1.x,p1.y
+		local u1,v1=uv[2*i-1],uv[2*i]
+		local _x1,_y1,_u1,_v1=x1,y1,u1,v1
+		if(y0>y1) x0,y0,x1,y1,u0,v0,u1,v1=x1,y1,x0,y0,u1,v1,u0,v0
+		local dy=y1-y0
+		local dx,du,dv=(x1-x0)/dy,(u1-u0)/dy,(v1-v0)/dy
+		local cy0=y0\1+1
+		if(y0<0) x0-=y0*dx u0-=y0*du v0-=y0*dv y0=0 cy0=0
+		-- sub-pix shift
+		local sy=cy0-y0
+		x0+=sy*dx
+		u0+=sy*du
+		v0+=sy*dv
+		if(y1>127) y1=127
+		for y=cy0,y1 do
+			local span=spans[y]
+			if span then
+				--rectfill(x[1],y,x0,y,offset/16)
+				
+				local a,au,av,b,bu,bv=x0,u0,v0,unpack(span)
+				if(a>b) a,au,av,b,bu,bv=b,bu,bv,a,au,av
+				local ca,cb=a\1+1,b\1
+				if ca<=cb then
+					-- pixel perfect sampling
+					local sa,dab=ca-a,b-a
+					local dau,dav=(bu-au)/dab,(bv-av)/dab
+					tline(ca,y,cb,y,au+sa*dau,av+sa*dav,dau,dav)
+				end
+			else
+				spans[y]={x0,u0,v0}
+			end
+			x0+=dx
+			u0+=du
+			v0+=dv
+		end
+		x0,y0,u0,v0=_x1,_y1,_u1,_v1
+	end
+end
