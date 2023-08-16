f2b7b3bc5ac0fd42f54644d03ecd1467a35878c7
freds <frederic.souchu@laposte.net>
2023-08-16 21:34:07 +0200

    added tpoly variant for flat polygons and perspective correct

--- carts/poly.lua
+++ carts/poly.lua
@@ -82,3 +82,54 @@ function tpoly(v,uv)
 		x0,y0,u0,v0=_x1,_y1,_u1,_v1
 	end
 end
+
+-- perpsective correct rasterization for polygons n=(0 1 0)
+function tpoly_flat(v,uv)
+	local p0,spans=v[#v],{}
+	local x0,y0,w0=p0.x,p0.y,p0.w
+	-- ensure proper interpolation in screen space
+	local u0,v0=uv[#uv-1]*w0,uv[#uv]*w0
+	-- ipairs is slower for small arrays
+	for i=1,#v do
+		local p1=v[i]
+		local x1,y1,w1=p1.x,p1.y,p1.w
+		local u1,v1=uv[2*i-1]*w1,uv[2*i]*w1
+		local _x1,_y1,_u1,_v1,_w1=x1,y1,u1,v1,w1
+		if(y0>y1) x0,y0,x1,y1,u0,v0,w0,u1,v1,w1=x1,y1,x0,y0,u1,v1,w1,u0,v0,w0
+		local dy=y1-y0
+		local dx,du,dv,dw=(x1-x0)/dy,(u1-u0)/dy,(v1-v0)/dy,(w1-w0)/dy
+		local cy0=y0\1+1
+		if(y0<0) x0-=y0*dx u0-=y0*du v0-=y0*dv w0-=y0*dw y0=0 cy0=0
+		-- sub-pix shift
+		local sy=cy0-y0
+		x0+=sy*dx
+		u0+=sy*du
+		v0+=sy*dv
+		w0+=sy*dw
+		if(y1>127) y1=127
+		for y=cy0,y1 do
+			local span=spans[y]
+			if span then
+				--rectfill(x[1],y,x0,y,offset/16)
+				
+				local x0,u0,v0,w0,x1,u1,v1,w1=x0,u0,v0,w0,unpack(span)
+				if(x0>x1) x0,u0,v0,w0,x1,u1,v1,w1=x1,u1,v1,w1,x0,u0,v0,w0
+				local rx,lx=x0\1+1,x1\1
+				if rx<=lx then
+					-- pixel perfect sampling
+					local sdx,dab=rx-x0,x1-x0
+					-- perspective correction
+					local du,dv=(u1/w1-u0/w0)/dab,(v1/w1-v0/w0)/dab
+					tline(rx,y,lx,y,u0/w0+sdx*du,v0/w0+sdx*dv,du,dv)
+				end
+			else
+				spans[y]={x0,u0,v0,w0}
+			end
+			x0+=dx
+			u0+=du
+			v0+=dv
+			w0+=dw
+		end
+		x0,y0,u0,v0,w0=_x1,_y1,_u1,_v1,_w1
+	end
+end
